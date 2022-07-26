6bdf4b97a09725851ef73accb9102d3801cc4e15
Débora Caires <87550053+deboracaires@users.noreply.github.com>
2022-07-26 11:09:24 -0300

    Update ButtonPerk.js
    removing console.log

--- src/components/ButtonPerk/ButtonPerk.js
+++ src/components/ButtonPerk/ButtonPerk.js
@@ -1,7 +1,6 @@
 import { Container } from "./Styleds";
 
 export default function ButtonPerk({ perk }) {
-  console.log(perk)
   return(
     <a href={perk.link} rel='noreferrer' target='_blank'>
       <Container>
